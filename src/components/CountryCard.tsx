1d04393253dfb9eca2f1cd6dc61da29ba88f73df
poop6028@naver.com <poop6028@naver.com>
2024-06-28 13:19:56 +0900

    과제 완성

---
+++ src/components/CountryCard.tsx
@@ -0,0 +1,22 @@
+import { Country } from "./../types/country";
+
+interface CountryCardProps {
+  country: Country;
+  handleSelectCountry: (country: Country) => void;
+}
+const CountryCard: React.FC<CountryCardProps> = ({
+  country,
+  handleSelectCountry,
+}) => {
+  return (
+    <div onClick={() => handleSelectCountry(country)}>
+      <img
+        src={country.flags.svg}
+        style={{ width: "40px", height: "40px", cursor: "pointer" }}
+      />
+      <h3>{country.name.common}</h3>
+    </div>
+  );
+};
+
+export default CountryCard;
